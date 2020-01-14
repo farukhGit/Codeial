41d1dc4f6d8083dfab45e315b0091c38414b89a4
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-15 03:02:19 +0530

    define the create action(signup)

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = (req, res)=>{
     return res.render('user', {
         title : 'User Profile'
@@ -18,7 +20,32 @@ module.exports.signIn = (req, res) =>{
 
 // sign up and create a document for the user
 module.exports.create = (req, res)=>{
-    // to do 
+    if(req.body.password != req.body.confirmPassword){
+        console.log('Passwords entered do not match!');
+        return res.redirect('back');
+    }
+
+    User.findOne({email : req.body.email}, (err, user)=>{
+        if(err){
+            console.log('Error in finding user.');
+            return; 
+        }
+
+        if(!user){
+            User.create(req.body, (err, user)=>{
+                if(err){
+                    console.log('Error in creating user while sign up.');
+                    return;
+                }
+
+                return res.redirect('/users/signin');
+            });
+        }else{
+            // if user exists, send back to the sign up page
+            console.log('User with same email already exists');
+            return res.redirect('back');
+        }
+    })
 }
 
 // sign in and create a session for the user
