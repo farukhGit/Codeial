071ce8a33a43721f3d121d2b1c5cbef3f68993f2
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-14 06:57:51 +0530

    create /routes/user_routes.js, /controllers/user_controller.js and add /users/ route in /routes/index.js

---
+++ controllers/users_controller.js
@@ -0,0 +1,3 @@
+module.exports.profile = (req, res)=>{
+    return res.end('<h1>User Profile - 1</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
+const userController = require('../controllers/users_controller');
 
 
 router.get('/', homeController.homeActionOne);
+router.use('/users', require('./user_routes.js'));
+
 
 module.exports = router;
