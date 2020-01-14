e0474f7e6537c02d436b09f8d88055dd36ef54b1
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-14 17:34:35 +0530

    create layout.ejs, include _header.ejs and _footer.ejs in layout, remove redundant code from home.ejs and user.ejs

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8002;
+const expressLayouts = require('express-ejs-layouts');
 
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// use ejs-layouts  
+app.use(expressLayouts);
 
 // use express router
 app.use('/', require('./routes/index'));
