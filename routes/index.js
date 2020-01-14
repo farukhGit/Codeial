96c5c6261fc64b1ae538e18139a4a06506ad3482
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-14 07:15:27 +0530

    move userController from index.js to user_routes.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-const userController = require('../controllers/users_controller');
 
 
 router.get('/', homeController.homeActionOne);
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
+const userController = require('../controllers/users_controller');
+
+
 router.get('/profile', require('../controllers/users_controller').profile);
 
 module.exports = router;
