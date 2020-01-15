3cb2e796ddf7a0b493cd891bcafebc1bebeb0ae2
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-15 15:57:38 +0530

    setup passportjs

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+passport.use(new LocalStrategy({
+    usernameField : email
+}, function(email, password, done){
+    User.fineOne({email : email}, function(err, user){
+        if(err){
+            console.log('Error in finding user -----> passport');
+            return done(err);
+        }
+        if(!user || user.password != password){
+            console.log('Invalid Username or Password');
+            return done(null, false);
+        }
+
+        return done(null, user);
+    });
+}));
+
+
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id, (err, user)=>{
+        if(err){
+            console.log('Error in finding user ---> passport');
+            return done(err);
+        }
+
+        return done(null, user);
+    });
+});
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = (req, res)=>{
     return res.render('user', {
         title : 'User Profile'
@@ -17,7 +19,32 @@ module.exports.signIn = (req, res) =>{
 }
 
 module.exports.create = (req, res)=>{
-    // to do 
+    // compare password and confirm password
+    if(req.body.password != req.body.confirmPassword){
+        console.log('Passwords do not match.');
+        return res.redirect('back');
+    }
+
+    // check email in database - see if already exists
+    User.findOne({email : req.body.email}, (err, user)=>{
+        if(err){
+            console.log('Error finding the user');
+            return res.redirect('back');
+        }
+        if(!user){
+            User.create(req.body, (err, user)=>{
+                if(err){
+                    console.log('Error creating a user.');
+                    return;
+                }
+                console.log('Success creating a new user.');
+                return res.redirect('/users/sign-in');
+            });
+        }else{
+            console.log('Email address already exists.');
+            return res.redirect('back');
+        }
+    });
 }
 
 module.exports.createSession = (req, res)=>{
