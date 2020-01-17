ff4bea1aabf2f526a9e60a23d5c94c231efcac6b
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-17 14:40:11 +0530

    save posts to database

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.homeActionOne);
 router.use('/users', require('./user_routes.js'));
+router.use('/posts', require('./posts_routes'));
 
 
 module.exports = router;
