895252d222e543d5985da1617c9105b991eef6aa
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-17 23:48:59 +0530

    delete posts (Authorised)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comments');
 
 module.exports.create = (req, res)=>{
     Post.create({
@@ -11,4 +12,18 @@ module.exports.create = (req, res)=>{
 
         return res.redirect('back');
     });
+}
+
+module.exports.destroy = (req, res)=>{
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the objectId into string
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post : req.params.id}, function(err){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
