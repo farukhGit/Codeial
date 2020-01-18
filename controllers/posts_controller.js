47291e908d0bffe4ff7517578ff86ab3ac3348a3
farukhGit <58646969+farukhGit@users.noreply.github.com>
2020-01-18 17:03:23 +0530

    make post-actions asyn/await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,29 +1,33 @@
 const Post = require('../models/post');
 const Comment = require('../models/comments');
 
-module.exports.create = (req, res)=>{
-    Post.create({
-        content : req.body.content,
-        user : req.user._id
-    },(err, post)=>{
-        if(err){
-            console.log('Error in creating a post.'); return;
-        }
-
+module.exports.create = async (req, res)=>{
+    try {
+        await Post.create({
+            content : req.body.content,
+            user : req.user._id
+        });
         return res.redirect('back');
-    });
+
+    } catch (error) {
+        console.log('Error : ', err);
+        return;
+    }
 }
 
-module.exports.destroy = (req, res)=>{
-    Post.findById(req.params.id, function(err, post){
-        // .id means converting the objectId into string
-        if(post.user == req.user.id){
-            post.remove();
-            Comment.deleteMany({post : req.params.id}, function(err){
-                return res.redirect('back');
-            });
-        }else{
-            return res.redirect('back');
-        }
-    })
+module.exports.destroy = async (req, res)=>{
+
+    try {
+       let post = await Post.find(req.params.id);
+       
+       if(post.user == req.user.id){
+        post.remove();
+
+        await Comment.deleteMany({post : req.user.id}); 
+        return res.redirect('back');
+       }
+    } catch (error) {
+        console.log('Error : ', err);
+        return;
+    }
 }
